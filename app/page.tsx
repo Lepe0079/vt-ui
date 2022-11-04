8e995462d6d7cdd0173069fc7c6d281d57dc5d5e
Dan Lepe <lepedaniel8822@gmail.com>
2022-11-04 09:48:04 -0500

    initial album load using external api

--- app/page.tsx
+++ app/page.tsx
@@ -5,47 +5,93 @@ import Card from "./components/card";
 
 export default function HomePage() {
   const [search, setSearch] = useState<string>('')
-  const [albums, setAlbums] = useState(null)
+  const [albums, setAlbums] = useState<any>([])
   
   useEffect(() => {
-    console.log(search)
+  
   }, [search])
 
   const handleChange = (e:any) => {
+
     setSearch(e.target.value)
   }
 
   const handleSubmit = (e:any) => {
+    e.preventDefault()
+    fetch(`http://localhost:8080/album/search/${search}`, {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setAlbums([...res.data.cached, ...res.data.remote]))
+      .catch((err) => console.error(err));
+  }
 
+  const handleSelect = (e:any) => {
+    console.log(e.target.attributes.id.value)
   }
 
   return (
-        <Card>
-          <form onSubmit={handleSubmit}>
-            <input
-              id="albumSearch"
-              type="text"
-              value={search}
-              onChange={handleChange}
-              placeholder="Album/Track"
-              className="border-2 border-cyan-800 p-2 px-8 rounded-2xl text-xl"
-            />
-            <button
-              id="search"
-              type="submit"
-              className="border-2 border-slate-500 p-2 rounded-2xl"
-            >
-              Search
-            </button>
-            <button
-              id="clear"
-              type="button"
-              onClick={() => setSearch('')}
-              className="bg-sky-800 border-2 border-slate-500 p-2 rounded-2xl"
-            >
-              Clear
-            </button>
-          </form>
-        </Card>
+    <div>
+      <Card>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="albumSearch"
+            type="text"
+            value={search}
+            onChange={handleChange}
+            placeholder="Album/Track"
+            className="border-2 border-cyan-800 p-2 px-8 rounded-2xl text-xl"
+          />
+          <button
+            id="search"
+            type="submit"
+            className="border-2 p-2 rounded-2xl bg-sky-800 border-slate-500 hover:text-black hover:bg-slate-500 hover:border-transparent"
+          >
+            Search
+          </button>
+          <button
+            id="clear"
+            type="button"
+            onClick={() => setSearch('')}
+            
+            className="border-2 p-2 rounded-2xl bg-sky-800 border-slate-500 hover:text-black hover:bg-slate-500 hover:border-transparent"
+          >
+            Clear
+          </button>
+        </form>
+      </Card>
+      {albums && (
+        <div className="grid gap-4 grid-cols-3">
+          {albums.map((album: any) => {  
+            return (
+              <div className="card flex grow justify-start max-w-4xl content-end space-x-4" key={album.vtName}>
+                <img src={album.thumbnail} width="128" height="128" />
+                <div className="md:container"> 
+                    <div className="place-self-start">
+                      <h1>{album.title}</h1>
+                      <h3>{album.year}</h3>
+                      <h6>{album.platforms.join(", ")}</h6>
+                    </div>
+                    <div className="pt-5">
+                      <button 
+                        id={album.vtName} 
+                        type="button" 
+                        onClick={handleSelect}  
+                        className="px-4 py-1 text-sm font-semibold rounded-full border bg-sky-800 border-slate-500 hover:text-black hover:bg-slate-500 hover:border-transparent"
+                        >
+                          Browse
+                      </button>
+                    </div>
+                </div>
+              </div>
+            )      
+          })}
+        </div>
+      )}
+    </div>
+      
   );
 }
